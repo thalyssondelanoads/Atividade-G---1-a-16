574632b16330374f43ba0e85a4278c0b320e509b
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-29 23:30:50 -0300

    Create questao3.py

---
+++ questao3.py
@@ -0,0 +1,18 @@
+def vogal_consoante():
+    vogais = 'aeiou'
+    
+    if letra in vogais:
+        print('A Letra é uma Vogal')
+    
+    else:
+        print('A Letra é uma Consoante')
+        
+#-------------------------------- 
+    
+print('Vogal Ou Consoante')
+print('------------')
+
+letra = str(input('Digite a Letra ( Minúscula ) que Deseja Conferir: '))
+print('------------')
+
+vogal_consoante()
