a174d87b00754de29e2301a8adef80119a62ed48
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-29 22:35:34 -0300

    Create questao1.py

---
+++ questao1.py
@@ -0,0 +1,16 @@
+def positivo_negativo():
+    if num > 0:
+        print('O Número é Positivo')
+    
+    elif num < 0:
+         print('O Número é Negativo')
+         
+#-------------------------------- 
+    
+print('Positivo Ou Negativo')
+print('------------')
+
+num = int(input('Digite o Número que Deseja Conferir: '))
+print('------------')
+
+positivo_negativo()
