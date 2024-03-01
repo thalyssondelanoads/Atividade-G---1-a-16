99b9c8043d2c5590c7d923a3081a95924045ea51
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-29 23:05:06 -0300

    Create questao2.py

---
+++ questao2.py
@@ -0,0 +1,19 @@
+def masc_femin():
+    if letra == 'F':
+        print('F - Feminino ')
+        
+    elif letra == 'M':
+        print('M - Masculino ')
+    
+    else:
+        print('Sexo Inválido')
+    
+#-------------------------------- 
+    
+print('Masculino Ou Feminino')
+print('------------')
+
+letra = str(input('Digite a Letra Chave | M ou F |: '))
+print('------------')
+
+masc_femin()
