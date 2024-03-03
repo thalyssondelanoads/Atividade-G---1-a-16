8456ccebb436757960ef8e8a4e7b157c15dc32d6
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-03 20:15:25 -0300

    Create questao12.py

---
+++ questao12.py
@@ -0,0 +1,17 @@
+def definir():
+
+    if num == int(num):
+        print('Inteiro')
+    
+    else:
+        print('Decimal')
+
+#------------------
+
+print('Inteiro ou Decimal')
+print('---------------')
+
+num = float(input('Digite o Número: '))
+print('---------------')
+
+definir()
