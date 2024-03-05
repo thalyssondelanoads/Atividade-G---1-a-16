f6fd281f8e039cd71ee6bd97f40304185467995f
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-05 20:58:03 -0300

    Update questao6.py

--- questao6.py
+++ questao6.py
@@ -1,1 +1,19 @@
-.
+def saudaçao():
+    if turno == 'M':
+        print('Bom Dia!')
+    elif turno == 'V':
+        print('Boa Tarde!')
+    elif turno == 'N':
+        print('Boa Noite!')
+    else:
+        print('Valor Inválido')
+        
+#---------------------------------
+
+print('Saudação')
+print('-----------')
+
+turno = str(input('Digite o seu Turno de Estudo ( M, V ou N ): '))
+print('-----------')
+
+saudaçao()
